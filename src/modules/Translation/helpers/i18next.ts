608d3f37095234b0d51267421397c6bad22a581a
Yaroslav Syvukha <yaroslav.syvukha@gmail.com>
2023-04-04 21:08:44 +0300

    fix: deploy

--- src/modules/Translation/helpers/i18next.ts
+++ src/modules/Translation/helpers/i18next.ts
@@ -1,13 +1,16 @@
 import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
-import I18NextHttpBackend from 'i18next-http-backend';
+import HttpBackend, { HttpBackendOptions } from 'i18next-http-backend';
 import { initReactI18next } from 'react-i18next';
 
 i18n
-  .use(I18NextHttpBackend)
+  .use(HttpBackend)
   .use(LanguageDetector)
   .use(initReactI18next)
-  .init({
+  .init<HttpBackendOptions>({
+    backend: {
+      loadPath: '/anime-portal/locales/{{lng}}/{{ns}}.json',
+    },
     fallbackLng: 'en',
     ns: ['common', 'auth'],
     interpolation: {
