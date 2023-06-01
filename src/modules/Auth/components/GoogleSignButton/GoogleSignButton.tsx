b5eee8b2ae7f5fe201f7c289764f1a21c1c6f0ad
Oleksandr Turchyn <turchinsasha4@gmail.com>
2023-06-01 22:23:47 +0300

    feat: google auth impl

--- src/modules/Auth/components/GoogleSignButton/GoogleSignButton.tsx
+++ src/modules/Auth/components/GoogleSignButton/GoogleSignButton.tsx
@@ -7,10 +7,20 @@ import {
 } from './styles';
 
 import googleSvg from '@/assets/sign/google.svg';
+import {
+  CLIENT_URL,
+  API_URL,
+  AUTHORIZATION_STORAGE_KEY,
+} from '@/constants/common';
 
 const GoogleSignButton: FC<PropsWithChildren> = ({ children }) => {
+  const handleClick = async () => {
+    localStorage.setItem(AUTHORIZATION_STORAGE_KEY, 'true');
+    window.location.href = `${API_URL}/google-auth/google?returnUrl=${CLIENT_URL}`;
+  };
+
   return (
-    <GoogleSignInButton href="#">
+    <GoogleSignInButton onClick={handleClick}>
       <GoogleLogo src={googleSvg} alt="" />
       <GoogleSignInButtonText>{children}</GoogleSignInButtonText>
     </GoogleSignInButton>
--- src/shared/constants/common.ts
+++ src/shared/constants/common.ts
@@ -3,6 +3,7 @@ export const API_URL = `${BASE_API_URL}/api`;
 export const AUTHORIZATION_STORAGE_KEY = 'authorized';
 export const AUTHORIZATION_TOKEN_STORAGE_KEY = 'auth_token';
 export const REFRESH_TOKEN_STORAGE_KEY = 'refresh_token';
+export const CLIENT_URL = import.meta.env.VITE_PUBLIC_CLIENT_URL ?? '';
 
 export const SAVED_ROUTE_LOCATION_DATA_STORAGE_KEY =
   'saved_route_location_data';
